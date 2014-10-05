fea9e11440e6269bd464dc9672edc62ea0826cee
Oliver Hennigh <loliverhennigh101@gmail.com>
2014-10-05 18:20:12 -0400

    it works a little

--- test_functions.jl
+++ test_functions.jl
@@ -17,6 +17,30 @@ function test_func_b(x)
 	return fittness
 end
 
+function test_func_c(x)
+
+	fittness = 0.0;
+	for n=1:size(x,1)
+		fittness = fittness + x[n] * (mod(n, 5) - 2);
+	end
+	return fittness
+end
+
+function test_func_d(x)
+	fittness = 0.0;
+	for n=1:size(x,1)
+		fittness = fittness + x[n] * (mod(n, 5) - 2);
+	end
+	for n=1:size(x,1)
+		fittness = fittness + x[n] * (mod(n, 11) - 5);
+	end
+	for n=1:size(x,1)
+		fittness = fittness + x[n] * (mod(n, 3) - 1);
+	end
+	return fittness
+end
+
+
 
 
 
