917d201a5cb8f94435883ccc59c3392e019c9a76
csguest <csguest@itl.clarkson.edu>
2014-11-10 20:47:35 -0500

    holy shit

--- RBM_search.jl
+++ RBM_search.jl
@@ -110,6 +110,72 @@ function RBM_search_hope(func, size_x, size_y, num_trys, learning_rate)
 	return best
 end
 
+
+# Possibly last chance 
+function RBM_search_chance(func, size_x, size_y, num_trys, learning_rate)
+
+	# rbm stuff
+	weight_matrix = .1 * (rand(Float64, size_x, size_y)) - .05; 
+	momentum_speed = fill(0.0, size(weight_matrix));
+	
+	# search variables
+	best = fill(0.0, num_trys)
+	test_y_1 = randbool(size_y,1);
+	test_x_1 = visible_to_hidden_state(test_y_1, weight_matrix);
+	test_y_2 = randbool(size_y,1);
+	test_x_2 = randbool(size_x,1);
+#	print(size(test_x), "\n");
+	store_1 = 0.0
+	store_2 = 0.0
+	store_choice = 1;
+	store_true = 0.0;
+	best[1] = func(test_x_1);
+	
+
+	# runs on hope and a prayer
+	for n=2:num_trys
+		
+		##### generate the vectors and stuff #####
+		test_y_1 = randbool(size_y, 1)
+		test_x_1 = visible_to_hidden_state(test_y_1, weight_matrix);
+		test_y_2 = hidden_to_visible_state(test_x_1, weight_matrix);
+		test_x_2 = visible_to_hidden_state(test_y_2, weight_matrix);
+		
+		##### generate see whether 1 or 2 is better #####
+		store_1 = func(test_x_1);
+		store_2 = func(test_x_2);
+
+		##### store the best #####
+		best[n] = best[n-1];
+		if (store_2 <= store_1)
+			store_true = store_1;
+			store_choice = 1;
+		end
+		if (store_1 <= store_2)
+			store_true = store_2;
+			store_choice = 2;
+		end
+		if (store_true >= best[n])
+			best[n] = store_true;
+		end
+		
+		##### now for the learning #####
+		if (store_choice == 1)
+			gradient = (goodness_gradient(test_y_1, test_x_1) - goodness_gradient(test_y_2, test_x_2));
+		end
+		if (store_choice == 2)
+			gradient = (- goodness_gradient(test_y_1, test_x_1) + goodness_gradient(test_y_2, test_x_2));
+		end
+		momentum_speed = .9 * momentum_speed + gradient';
+		weight_matrix += momentum_speed * learning_rate;
+	end
+
+	return best
+end
+
+
+
+
 function gradient_special(test_y, test_x, weight_matrix)
 	# calc visible state from hidden state
 	visible_batch_2 = hidden_to_visible_state(test_x, weight_matrix);
